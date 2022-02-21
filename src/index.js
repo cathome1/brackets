a45454be3d31204ac30f6fe897683859d2074aff
cathome1 <cathome.bro@yandex.com>
2022-02-22 02:56:31 +0300

    feat: make func

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
+module.exports = function check(str, brackets) {
+  let arr = []
+  for (let i of str) {
+    if (i !== new Map(brackets).get(arr[arr.length - 1])) arr.push(i)
+    else arr.pop(i)
+  }
+  return !arr.length
 }
